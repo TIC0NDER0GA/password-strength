807c8476cea597deea1145aaada110ba273f8778
dey60704 <dey60704@uga.edu>
2021-10-11 20:42:27 -0400

    tkinter file added
    Signed-off-by: dey60704 <dey60704@uga.edu>

--- PasswordGui.py
+++ PasswordGui.py
@@ -1,4 +1,8 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title('PassWord Strength Checker') # Changes the title of the window
+root.geometry('600x600+50+50') # Changes the sze of the window
+root.resizable(True) # Allows 
+
 root.mainloop()
