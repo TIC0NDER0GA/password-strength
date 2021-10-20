bad577747e10adffbdfb0138f81b71573aba3a62
dey60704 <dey60704@uga.edu>
2021-10-20 17:44:53 -0400

    added methods to PasswordStrength.py made a draft of the PasswordGui.py that is somewhat functional.
    Signed-off-by: dey60704 <dey60704@uga.edu>

--- PasswordStrength.py
+++ PasswordStrength.py
@@ -7,7 +7,8 @@ class PassStrength:
     # 1b. There are several things that make a bad password
     # 2b. Using words,
     # names, a group of numbers (birthday: 0801000), short password
-    #commitance
+    # commitance
+
         def __init__(self):
             self.password = ""
 
@@ -23,7 +24,25 @@ class PassStrength:
                         num = True
             return num
 
-        def lowerCase(self, letter:str):
+
+        def numOfNum(self, p:str):
+            count = 0;
+            for x in p:
+                if (self.isANum(x)):
+                    count = count + 1
+            return count
+
+
+        def isANum(self, p:str):
+            num = False;
+            numList = list(range(47,59))
+            asciiVal = ord(p)
+            if (numList.count(asciiVal) == 1):
+                return True
+            else:
+                return num;
+
+        def isLowerCase(self, letter:str):
             lower = False
             lowList = list(range(97, 123))
             asciiVal = ord(letter)
@@ -32,3 +51,46 @@ class PassStrength:
                 return True
             else:
                 return lower
+
+        def isUpperCase(self, letter: str):
+            lower = False
+            lowList = list(range(65, 91))
+            asciiVal = ord(letter)
+
+            if (lowList.count(asciiVal) == 1):
+                return True
+            else:
+                return lower
+
+
+        def lowerCaseWhole(self, letter: str):
+            checker = False
+            for x in letter:
+                if (self.isLowerCase(x) == True):
+                    checker = True
+            return checker
+
+
+        def upperCaseWhole(self, letter: str):
+            checker = False
+            for x in letter:
+                if (self.isUpperCase(x) == True):
+                    checker = True
+            return checker
+
+        def numOfUpper(self, letter:str):
+            count = 0;
+            for x in letter:
+                if (self.isUpperCase(x)):
+                    count = count + 1
+            return count
+
+
+        def numOfLower(self, p:str):
+            count = 0;
+            for x in p:
+                if (self.isLowerCase(x)):
+                    count = count + 1
+
+            return count
+
